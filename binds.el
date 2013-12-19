992430d43f29c88264fede84246d595589d2b32a
Cody Hanika <chanika@iastate.edu>
2013-12-18 20:05:02 -0600

    Added binding for magit

--- binds.el
+++ binds.el
@@ -1,4 +1,5 @@
 (global-set-key (kbd "<f8>") 'shell)
+(global-set-key (kbd "<f7>") 'magit-status)
 ;; invoke meta-x 
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
