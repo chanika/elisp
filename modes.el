79b06e55d395d3c1fcf996662a092c36cf058695
Cody Hanika <chanika@iastate.edu>
2014-01-09 15:36:27 -0600

    Added semantic support

--- modes.el
+++ modes.el
@@ -17,3 +17,41 @@
 
 ;; .h files open in c++-mode
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;;CEDET
+
+(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-mru-bookmark-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-local-symbol-highlight-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-decoration-mode)
+
+(require 'semantic/ia)
+(semantic-mode 1)
+
+(require 'semantic/bovine/gcc)
+
+;; ;; Imenu integration
+(defun my-semantic-hook ()
+  (imenu-add-to-menubar "TAGS"))
+(add-hook 'semantic-init-hooks 'my-semantic-hook)
+
+;; Class suggest improvement
+(defun my-c-mode-cedet-hook ()
+ (local-set-key "." 'semantic-complete-self-insert)
+ (local-set-key ">" 'semantic-complete-self-insert))
+(add-hook 'c-mode-common-hook 'my-c-mode-cedet-hook)
+
+;;;; Semantic and auto-config integratio
+(add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-20131128.233/")    ; This may not be appeared if you have already added.
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+(define-key ac-mode-map [(meta return)] 'auto-complete)
+
+(defun my-cedet-hook ()
+  (add-to-list 'ac-sources 'ac-source-semantic))
+(add-hook 'c-mode-common-hook 'my-cedet-hook)
