d62c19eff52e960393fcc73d536ffcadbb1dbccc
Cody Hanika <codyhanika@gmail.com>
2014-01-09 15:55:21 -0600

    Return is now newline-and-indent in c-modes

--- binds.el
+++ binds.el
@@ -3,3 +3,6 @@
 ;; invoke meta-x 
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
+;; c-mode auto-indent
+(add-hook 'c-mode-common-hook '(lambda ()
+  (local-set-key (kbd "RET") 'newline-and-indent)))
