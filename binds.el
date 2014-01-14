2ac37456c1e736d590be6e3d31830a5a08cf58e9
Cody Hanika <chanika@iastate.edu>
2014-01-14 14:12:18 -0600

    Added 'other-window bind

--- binds.el
+++ binds.el
@@ -1,8 +1,10 @@
 (global-set-key (kbd "<f8>") 'shell)
 (global-set-key (kbd "<f7>") 'magit-status)
+(global-set-key (kbd "C-;") 'other-window)
 ;; invoke meta-x 
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 ;; c-mode auto-indent
 (add-hook 'c-mode-common-hook '(lambda ()
   (local-set-key (kbd "RET") 'newline-and-indent)))
+
