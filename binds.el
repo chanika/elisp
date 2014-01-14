5e99d3d3e3279cd7dfe168cfc4c044963d5652c9
Cody Hanika <chanika@iastate.edu>
2014-01-14 15:10:51 -0600

    Moved keybinds around for movement

--- binds.el
+++ binds.el
@@ -7,18 +7,17 @@
 (add-hook 'c-mode-common-hook '(lambda ()
   (local-set-key (kbd "RET") 'newline-and-indent)))
 ;; movement
-(global-set-key (kbd "C-i") 'previous-line)
-(global-set-key (kbd "C-k") 'next-line)
-(global-set-key (kbd "C-j") 'backward-char)
-(global-set-key (kbd "C-l") 'forward-char)
+(global-set-key (kbd "M-i") 'previous-line)
+(global-set-key (kbd "M-k") 'next-line)
+(global-set-key (kbd "M-j") 'backward-char)
+(global-set-key (kbd "M-l") 'forward-char)
 ;; kill / yank
-(global-set-key (kbd "C-o") 'kill-line)
 (global-set-key (kbd "C-r") 'backward-kill-word)
 ;; screen movement
-(global-set-key (kbd "C-;") 'other-window)
-(global-set-key (kbd "C-m") 'recenter-top-bottom)
+(global-set-key (kbd "C-'") 'other-window)
 ;; search
 (global-set-key (kbd "C-f") 'isearch-forward)
 (global-set-key (kbd "C-s") 'isearch-backward)
-;; miscellaneous replacements
-(global-set-key (kbd "C-n") 'open-line)
+(define-key isearch-mode-map (kbd "C-f") 'isearch-repeat-forward)
+(define-key isearch-mode-map (kbd "C-s") 'isearch-repeat-backward)
+
