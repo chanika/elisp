7b7df9ca753d383fa5ffc9c0c0399a2aa54bda70
Cody Hanika <chanika@iastate.edu>
2014-01-23 22:32:55 -0600

    Added function to run virtutrace

--- vt-funcs.el
+++ vt-funcs.el
@@ -3,3 +3,11 @@
 (defun mvt ()
   (interactive)
   (compile "cd ~/git/virtutrace/build/ && make -j7 && cd -"))
+
+(defun rvt ()
+  (interactive)
+  (setq currentBuff (buffer-name))
+  (switch-to-buffer-other-window (shell "vt-out-shell"))
+  (insert "~/git/virtutrace/build/virtutrace/virtutrace $VTDATA/conf/virtutrace-standalone.xml")
+  (comint-send-input)
+  (switch-to-buffer-other-window currentBuff))
