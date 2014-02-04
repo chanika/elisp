495a662c06aaeda8a4561e389eed5800eee46705
Cody Hanika <codyhanika@gmail.com>
2014-02-03 21:41:55 -0600

    Fixed rvt to run if buffer isn't at point-max

--- vt-funcs.el
+++ vt-funcs.el
@@ -8,6 +8,8 @@
   (interactive)
   (setq currentBuff (buffer-name))
   (switch-to-buffer-other-window (shell "vt-out-shell"))
+  (point-max)
+  (comint-send-input)
   (insert "~/git/virtutrace/build/virtutrace/virtutrace $VTDATA/conf/virtutrace-standalone.xml")
   (comint-send-input)
   (switch-to-buffer-other-window currentBuff))
