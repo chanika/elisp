cf31808043e2acf0d0eea051d29879207bb840b7
Cody Hanika <chanika@iastate.edu>
2014-02-05 19:56:21 -0600

    Highlight tags in web-mode

--- modes.el
+++ modes.el
@@ -65,6 +65,8 @@
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 
+(setq web-mode-enable-current-element-highlight t)
+
 ;; SVN
 
 (require 'dsvn)
