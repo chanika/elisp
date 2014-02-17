c9a47f14854205f2e135247e09cce13875d42305
Cody Hanika <chanika@iastate.edu>
2014-02-17 14:52:22 -0600

    Fixed class access-label indents in c++

--- modes.el
+++ modes.el
@@ -5,9 +5,10 @@
     (c-set-offset 'innamespace '0)
     (c-set-offset 'inextern-lang '0)
     (c-set-offset 'inline-open '0)
+    (c-set-offset 'inclass '+)
+    (c-set-offset 'access-label '-)
     (c-set-offset 'label '*)
     (c-set-offset 'case-label '*)
-    (c-set-offset 'access-label '/)
     (setq c-basic-offset 4)
     (setq tab-width 4)
     (setq indent-tabs-mode nil)
@@ -67,6 +68,10 @@
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 
 (setq web-mode-enable-current-element-highlight t)
+
+(custom-set-variables
+  '(js2-basic-offset 4)
+  '(js2-bounce-indent-p t))
 
 ;; SVN
 
