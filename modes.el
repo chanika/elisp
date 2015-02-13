cd5bf88efc99da3c7b321e13bd990695e616f58f
Cody Hanika <codyhanika@gmail.com>
2015-02-12 21:47:20 -0600

    Updated for use-package

--- modes.el
+++ modes.el
@@ -2,6 +2,9 @@
 
 (setq-default indent-tabs-mode nil)
 
+(use-package magit
+    :ensure t)
+
 ;; c++-mode
 ;; set indentation style
 (defun my-cc-style()
@@ -15,8 +18,7 @@
     (c-set-offset 'case-label '*)
     (setq c-basic-offset 4)
     (setq tab-width 4)
-    (setq indent-tabs-mode nil)
-)
+    (setq indent-tabs-mode nil))
 
 (add-hook 'c++-mode-hook 'my-cc-style)
 
@@ -34,10 +36,9 @@
 (add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode)
 (add-to-list 'semantic-default-submodes 'global-semantic-decoration-mode)
 
-(require 'semantic/ia)
+(use-package semantic
+    :ensure t)
 (semantic-mode 1)
-
-(require 'semantic/bovine/gcc)
 
 ;; ;; Imenu integration
 (defun my-semantic-hook ()
@@ -52,7 +53,8 @@
 
 ;;;; Semantic and auto-config integratio
 (add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-20131128.233/")    ; This may not be appeared if you have already added.
-(require 'auto-complete-config)
+(use-package auto-complete
+    :ensure t)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
 (define-key ac-mode-map [(meta return)] 'auto-complete)
@@ -61,11 +63,11 @@
   (add-to-list 'ac-sources 'ac-source-semantic))
 (add-hook 'c-mode-common-hook 'my-cedet-hook)
 
-
 ;; web-mode
 
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(use-package web-mode
+    :ensure t)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
@@ -76,90 +78,10 @@
   (setq web-mode-code-indent-offset 4))
 (add-hook 'web-mode-hook 'my-web-mode-settings-hook)
 
-;; js2-mode
-
-(require 'js2-mode)
-;; fix indentation, thanks to http://rawsyntax.com/blog/learn-emacs-editing-javascript/
-(defun my-js2-indent-function ()
-  (interactive)
-  (save-restriction
-    (widen)
-    (let* ((inhibit-point-motion-hooks t)
-           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
-           (offset (- (current-column) (current-indentation)))
-           (indentation (js--proper-indentation parse-status))
-           node)
- 
-      (save-excursion
- 
-        (back-to-indentation)
-        ;; consecutive declarations in a var statement are nice if
-        ;; properly aligned, i.e:
-        ;;
-        ;; var foo = "bar",
-        ;;     bar = "foo";
-        (setq node (js2-node-at-point))
-        (when (and node
-                   (= js2-NAME (js2-node-type node))
-                   (= js2-VAR (js2-node-type (js2-node-parent node))))
-          (setq indentation ( 4 indentation))))
- 
-      (indent-line-to indentation)
-      (when (> offset 0) (forward-char offset)))))
- 
-(defun my-indent-sexp ()
-  (interactive)
-  (save-restriction
-    (save-excursion
-      (widen)
-      (let* ((inhibit-point-motion-hooks t)
-             (parse-status (syntax-ppss (point)))
-             (beg (nth 1 parse-status))
-             (end-marker (make-marker))
-             (end (progn (goto-char beg) (forward-list) (point)))
-             (ovl (make-overlay beg end)))
-        (set-marker end-marker end)
-        (overlay-put ovl 'face 'highlight)
-        (goto-char beg)
-        (while (< (point) (marker-position end-marker))
-          ;; don't reindent blank lines so we don't set the "buffer
-          ;; modified" property for nothing
-          (beginning-of-line)
-          (unless (looking-at "\\s-*$")
-            (indent-according-to-mode))
-          (forward-line))
-        (run-with-timer 0.5 nil '(lambda(ovl)
-                                   (delete-overlay ovl)) ovl)))))
- 
-(defun my-js2-mode-hook ()
-  (require 'js)
-  (setq js-indent-level 4
-        indent-tabs-mode nil
-        c-basic-offset 4)
-  (c-toggle-auto-state 0)
-  (c-toggle-hungry-state 1)
-  (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
-  (define-key js2-mode-map [(meta control |)] 'cperl-lineup)
-  (define-key js2-mode-map [(meta control \;)] 
-    '(lambda()
-       (interactive)
-       (insert "/* -----[ ")
-       (save-excursion
-         (insert " ]----- */"))
-       ))
-  (define-key js2-mode-map [(return)] 'newline-and-indent)
-  (define-key js2-mode-map [(backspace)] 'c-electric-backspace)
-  (define-key js2-mode-map [(control d)] 'c-electric-delete-forward)
-  (define-key js2-mode-map [(control meta q)] 'my-indent-sexp)
-  (if (featurep 'js2-highlight-vars)
-    (js2-highlight-vars-mode))
-  (message "My JS2 hook"))
- 
-(add-hook 'js2-mode-hook 'my-js2-mode-hook)
-
 ;; SVN
 
-(require 'dsvn)
+(use-package dsvn
+    :ensure t)
 
 ;; EDiff
 
