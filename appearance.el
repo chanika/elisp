7dfda17b9ad6c81d242b6968c58feb8ee456aaba
Cody Hanika <codyhanika@gmail.com>
2015-02-12 21:54:42 -0600

    Fixed linux full screen on open

--- appearance.el
+++ appearance.el
@@ -13,10 +13,8 @@
 (defun toggle-fullscreen ()
   (interactive)
   (if (eq system-type 'gnu/linux)
-  ((x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-	    		 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-	    		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
+      (custom-set-variables
+       '(initial-frame-alist (quote ((fullscreen . maximized)))))
   (w32-send-sys-command 61488)))
 (toggle-fullscreen)
 
